28c417caae5b07dcee08a11c191c960208d1cabb
manish076 <manishku073@gmail.com>
2025-12-05 16:42:01 +0530

    [FEAT] GET RFP Sent List Route

---
+++ backend/src/controller/getrfplist.controller.ts
@@ -0,0 +1,28 @@
+import { Request,Response } from "express";
+import { StatusCode } from "../schema/statuscode.enum";
+import prisma from "../utils/prisma.client";
+
+export const getrfplist=async(req:Request,res:Response)=>{
+    try{
+        const response=await prisma.rfps.findMany({
+            select:{
+                id:true,
+                raw_text:true
+            }
+        })
+        if(response.length==0){
+            return res.status(StatusCode.BAD_REQUEST).json({
+                message:"No Rfp Proposal Has Been Created"
+            })
+        }
+        return res.status(StatusCode.OK).json({
+            message:"Success",
+            data:response
+        })
+    }catch(e:any){
+        console.log("Internal Error While Fetching the Rpf List",e)
+        return res.status(StatusCode.INTERNAL_SERVER_ERROR).json({
+            message:"Internal Error While Fetching The RFP list"
+        })
+    }
+}
--- backend/src/routes/rfp.routes.ts
+++ backend/src/routes/rfp.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createRfp } from "../controller/create.controller";
 import { sendrfpmail } from "../controller/sendrpfmail.controller";
 import { comparevendor } from "../controller/comparevendor.controller";
+import { getrfplist } from "../controller/getrfplist.controller";
 
 const router:Router=Router();
 
@@ -9,5 +10,6 @@ const router:Router=Router();
 router.post("/create-rfp",createRfp);
 router.post("/:rfp_id/send",sendrfpmail)
 router.get("/:rfp_id/compare",comparevendor)
+router.get("/getrfp",getrfplist)
 
 export default router
